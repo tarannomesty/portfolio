22f3a25042db477c3ecaaa24ff8bd114ecbcffbe
tarannomesty <86516933+tarannomesty@users.noreply.github.com>
2021-09-30 16:49:02 +0600

    portfolio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+//typing animation script
+var typed=new Typed(".typing",{
+	strings: ["Web Developer", "Blogger", "Web Designer", "Freelancer"],
+	typeSpeed: 100,
+	backSpeed: 60,
+	loop: true
+
+});
